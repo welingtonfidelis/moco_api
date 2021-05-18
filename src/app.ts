c378968843a90f105633999fa00343a38e9c2725
Welington Fidelis <welingtonfidelis@gmail.com>
2021-05-18 08:15:46 -0300

    Include cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ import { router } from './routes';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', router);
 
 export {
